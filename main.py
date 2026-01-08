4de772e7af3c4776b54eda74d507db844dea6f67
swawsman69 <152324083+swawsman69@users.noreply.github.com>
2026-01-08 16:23:03 -0700

    Update main.py
    v0.2

--- main.py
+++ main.py
@@ -29,5 +29,8 @@ elif decrypt_or_encrypt == "e":
 
   print(f"original message : {plain_text}")
   print(f"encrypted message: {cipher_text}")
+  view = input ("do you want to share the encryption key? (y/n): ")
+if view == "y":
+    print ("step one: open the file tab on the left that says last_key .json. step two select all of the text and copy it. step three e-mail that text to the person you want to share it with. to input the new text go back to last_key.json and delete all of the old text and paste in the new ones.")
 else:
   print("ERROR!!! wrong input.")
